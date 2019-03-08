047506a5e2ff958cbc41eafefdd23ecff63d26ea
heybetzadec <heybetzadec@gmail.com>
2019-03-09 00:40:16 +0400

    update todo

--- src/main/java/com/todo/rest/service/restfulwebservice/TodoHardcodeService.java
+++ src/main/java/com/todo/rest/service/restfulwebservice/TodoHardcodeService.java
@@ -22,6 +22,17 @@ public class TodoHardcodeService {
         return todos;
     }
 
+    public Todo save(Todo todo){
+        if(todo.getId() == -1){
+            todo.setId(++idCounter);
+            todos.add(todo);
+        } else {
+            deleteById(todo.getId());
+            todos.add(todo);
+        }
+        return todo;
+    }
+
     public Todo deleteById(long id) {
         Todo todo = findById(id);
         if (todo==null)
@@ -31,7 +42,7 @@ public class TodoHardcodeService {
         return null;
     }
 
-    private Todo findById(long id) {
+    public Todo findById(long id) {
         for(Todo todo: todos) {
             if(todo.getId() == id)
                 return todo;
--- src/main/java/com/todo/rest/service/restfulwebservice/TodoResource.java
+++ src/main/java/com/todo/rest/service/restfulwebservice/TodoResource.java
@@ -1,6 +1,7 @@
 package com.todo.rest.service.restfulwebservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,14 +19,29 @@ public class TodoResource {
         return todoService.findAll();
     }
 
+    @GetMapping("/users/{username}/todos/{id}")
+    public Todo getTodo(@PathVariable String username, @PathVariable long id){
+        return todoService.findById(id);
+    }
+
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
         Todo todo = todoService.deleteById(id);
         if (todo != null) {
             return ResponseEntity.noContent().build();
         }
-
         return ResponseEntity.notFound().build();
     }
+
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(
+            @PathVariable String username,
+            @PathVariable long id, @RequestBody Todo todo
+    ){
+        Todo todoUpdated = todoService.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
 
 }
